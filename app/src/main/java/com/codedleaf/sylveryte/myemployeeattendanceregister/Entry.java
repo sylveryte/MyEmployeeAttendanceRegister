27f44c767e1926dbd9e61ba32e8b8e328d66bb97
dexoref <dexoref@gmail.com>
2016-06-12 05:11:14 +0530

    intial model

---
+++ app/src/main/java/com/codedleaf/sylveryte/myemployeeattendanceregister/Entry.java
@@ -0,0 +1,72 @@
+package com.codedleaf.sylveryte.myemployeeattendanceregister;
+
+import java.util.Date;
+import java.util.UUID;
+
+/**
+ * Created by sylveryte on 12/6/16.
+ */
+public class Entry {
+
+
+    public static final int PRESENT=70;
+    public static final int LATE=71;
+    public static final int HALF_TIME=72;
+    public static final int OVER_TIME=73;
+    public static final int ABSENT=74;
+
+
+    Date mDate;
+    int mRemark;
+    UUID mSiteId;
+    UUID mEmployeeId;
+    String mNote;
+
+
+    public Entry()
+    {
+        mDate=new Date();
+        mRemark=PRESENT;
+
+    }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date date) {
+        mDate = date;
+    }
+
+    public int getRemark() {
+        return mRemark;
+    }
+
+    public void setRemark(int remark) {
+        mRemark = remark;
+    }
+
+    public UUID getSiteId() {
+        return mSiteId;
+    }
+
+    public void setSiteId(UUID siteId) {
+        mSiteId = siteId;
+    }
+
+    public UUID getEmployeeId() {
+        return mEmployeeId;
+    }
+
+    public void setEmployeeId(UUID employeeId) {
+        mEmployeeId = employeeId;
+    }
+
+    public String getNote() {
+        return mNote;
+    }
+
+    public void setNote(String note) {
+        mNote = note;
+    }
+}
