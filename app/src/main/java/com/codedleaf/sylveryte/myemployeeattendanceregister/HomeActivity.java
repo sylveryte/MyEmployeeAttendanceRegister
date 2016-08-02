6dc16e4dac776238a10cd4576b9ea763dcd1501c
dexoref <dexoref@gmail.com>
2016-08-02 23:45:51 +0530

    added bottom bar

--- app/src/main/java/com/codedleaf/sylveryte/myemployeeattendanceregister/HomeActivity.java
+++ app/src/main/java/com/codedleaf/sylveryte/myemployeeattendanceregister/HomeActivity.java
@@ -1,12 +1,10 @@
 package com.codedleaf.sylveryte.myemployeeattendanceregister;
 
 import android.os.Bundle;
-import android.support.design.widget.TabLayout;
+import android.support.annotation.IdRes;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.app.FragmentStatePagerAdapter;
-import android.support.v4.view.ViewPager;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -19,6 +17,8 @@ import com.codedleaf.sylveryte.myemployeeattendanceregister.Editing.SiteAddition
 import com.codedleaf.sylveryte.myemployeeattendanceregister.GeneralFragments.DesignationFragment;
 import com.codedleaf.sylveryte.myemployeeattendanceregister.GeneralFragments.EmployeeFragment;
 import com.codedleaf.sylveryte.myemployeeattendanceregister.GeneralFragments.SitesFragment;
+import com.roughike.bottombar.BottomBar;
+import com.roughike.bottombar.OnMenuTabClickListener;
 
 /**
  * Created by sylveryte on 27/6/16.
@@ -37,34 +37,21 @@ public class HomeActivity extends AppCompatActivity {
 
     private com.getbase.floatingactionbutton.FloatingActionsMenu mMenu;
 
+    private FragmentManager mmFragmentManagerr;
 
-    /**
-     * The {@link android.support.v4.view.PagerAdapter} that will provide
-     * fragments for each of the sections. We use a
-     * {@link FragmentPagerAdapter} derivative, which will keep every
-     * loaded fragment in memory. If this becomes too memory intensive, it
-     * may be best to switch to a
-     * {@link android.support.v4.app.FragmentStatePagerAdapter}.
-     */
-    private SectionsPagerAdapter mSectionsPagerAdapter;
-
-    /**
-     * The {@link ViewPager} that will host the section contents.
-     */
-    private ViewPager mViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_home);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
 
         final com.getbase.floatingactionbutton.FloatingActionButton addSite=(com.getbase.floatingactionbutton.FloatingActionButton)findViewById(R.id.floating_add_site);
-        com.getbase.floatingactionbutton.FloatingActionButton addDesignation=(com.getbase.floatingactionbutton.FloatingActionButton)findViewById(R.id.floating_add_designation);
-        com.getbase.floatingactionbutton.FloatingActionButton addEmployee=(com.getbase.floatingactionbutton.FloatingActionButton)findViewById(R.id.floating_add_employee);
+        final com.getbase.floatingactionbutton.FloatingActionButton addDesignation=(com.getbase.floatingactionbutton.FloatingActionButton)findViewById(R.id.floating_add_designation);
+        final com.getbase.floatingactionbutton.FloatingActionButton addEmployee=(com.getbase.floatingactionbutton.FloatingActionButton)findViewById(R.id.floating_add_employee);
 
 
         mMenu=(com.getbase.floatingactionbutton.FloatingActionsMenu)findViewById(R.id.floating_add_menu);
@@ -77,7 +64,6 @@ public class HomeActivity extends AppCompatActivity {
                 }
             });
         }
-
         //floating button
         if (addDesignation != null) {
             addDesignation.setOnClickListener(new View.OnClickListener() {
@@ -87,7 +73,6 @@ public class HomeActivity extends AppCompatActivity {
                 }
             });
         }
-
         //floating button
         if (addEmployee != null) {
             addEmployee.setOnClickListener(new View.OnClickListener() {
@@ -98,27 +83,60 @@ public class HomeActivity extends AppCompatActivity {
             });
         }
 
+        mmFragmentManagerr = getSupportFragmentManager();
 
-        // Create the adapter that will return a fragment for each of the three
-        // primary sections of the activity.
-        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+        BottomBar bottomBar = BottomBar.attach(this, savedInstanceState);
+        bottomBar.setItems(R.menu.menu_bottom);
+        bottomBar.setOnMenuTabClickListener(new OnMenuTabClickListener() {
+                                                 @Override
+                                                 public void onMenuTabSelected(@IdRes int menuItemId) {
+                                                     switch (menuItemId)
+                                                     {
+                                                         case R.id.employee_bottomo_menu :
+                                                         {
+                                                              startFragment(EmployeeFragment.newInstance());
+                                                             break;
+                                                         }
+                                                         case R.id.site_bottom_menu :
+                                                         {
+                                                             startFragment(SitesFragment.newInstance());
+                                                             break;
+                                                         }
+                                                         case R.id.designation_bottom_menu :
+                                                         {
+                                                             startFragment(DesignationFragment.newInstance());
+                                                             break;
+                                                         }
+                                                     }
+                                                 }
 
-        // Set up the ViewPager with the sections adapter.
-        mViewPager = (ViewPager) findViewById(R.id.container);
-        mViewPager.setAdapter(mSectionsPagerAdapter);
+                                                 @Override
+                                                 public void onMenuTabReSelected(@IdRes int menuItemId) {
 
-        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
-        tabLayout.setupWithViewPager(mViewPager);
-        mViewPager.setCurrentItem(1);
+                                                     // TODO: 2/8/16 code to go to top of the fragment
+                                                 }
+                                             });
+
+
+        //for some reason this aint working
+        bottomBar.mapColorForTab(0, ContextCompat.getColor(this,R.color.colorEmployeeCard));
+        bottomBar.mapColorForTab(1, ContextCompat.getColor(this,R.color.colorSiteCard));
+        bottomBar.mapColorForTab(2, ContextCompat.getColor(this,R.color.colorDesignationCard));
     }
 
 
+    private void startFragment(Fragment fragment) {
+        if (fragment == null) {
+            fragment=SitesFragment.newInstance();
+            }
+            mmFragmentManagerr.beginTransaction()
+                    .add(R.id.fragment_container_home, fragment)
+                    .commit();
+    }
 
     private void startAddEmployee() {
         EmployeeAdditionFragmentDialog.getDialogFrag(null)
                 .show(getSupportFragmentManager(),DIALOG_FRAGMENT_CODE);
-
-
         closeFloatingMenu();
     }
 
@@ -183,51 +201,6 @@ public class HomeActivity extends AppCompatActivity {
             }
         }
         return super.onOptionsItemSelected(item);
-    }
-
-    /**
-     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
-     * one of the sections/tabs/pages.
-     */
-    public class SectionsPagerAdapter extends FragmentStatePagerAdapter {
-
-        public SectionsPagerAdapter(FragmentManager fm) {
-            super(fm);
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-            // getItem is called to instantiate the fragment for the given page.
-            // Return a PlaceholderFragment (defined as a static inner class below).
-            switch (position) {
-                case 0:
-                    return EmployeeFragment.newInstance();
-                case 1:
-                    return SitesFragment.newInstance();
-                case 2:
-                    return DesignationFragment.newInstance();
-            }
-            return null;
-        }
-
-        @Override
-        public int getCount() {
-            // Show 3 total pages.
-            return 3;
-        }
-
-        @Override
-        public CharSequence getPageTitle(int position) {
-            switch (position) {
-                case 0:
-                    return "Employee";
-                case 1:
-                    return "Site";
-                case 2:
-                    return "Designation";
-            }
-            return null;
-        }
     }
 
 }
